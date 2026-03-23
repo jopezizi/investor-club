9df92e98894e42a9e7667ab2c44f7c3ed25d314b
Joona Kääriäinen <kaariainenjoona@gmail.com>
2026-03-23 16:48:44 +0200

    Added primitive login system and started configuring secret key and session control

--- app.py
+++ app.py
@@ -1,14 +1,49 @@
-from flask import Flask
+from flask import  Flask, render_template, redirect, request, session
+import sqlite3
+import config
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
+app.secret_key = config.secret_key
+database_file = 'database.db'
+
+def init_database():
+    with sqlite3.connect(database_file) as db:
+        db.execute('''
+                   CREATE TABLE IF NOT EXISTS users (
+                   id INTEGER PRIMARY KEY,
+                   username TEXT UNIQUE,
+                   password_hash TEXT
+                   )''')
+        db.commit()
+
+init_database()
 
 @app.route('/')
 def index():
-    return "Heipparallaa!"
+    return render_template('index.html')
 
-@app.route('/counter')
-def test():
-    cont = ''
-    for i in range(100):
-        cont += str(i)+' '
-    return cont
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/create', methods=['POST'])
+def create():
+    username = request.form['username']
+    password1 = request.form['password1']
+    password2 = request.form['password2']
+    if password1 != password2:
+        return 'Salasanat eivät täsmää'
+    password_hash = generate_password_hash(password1)
+    
+    try:
+        with sqlite3.connect(database_file) as db:
+            sql = 'INSERT INTO users (username, password_hash) VALUES (?,?)'
+            db.execute(sql, [username, password_hash])
+            db.commit()
+    except sqlite3.IntegrityError:
+        return 'Käyttäjätunnus varattu.'
+    
+    return 'Käyttäjätunnus luotu.'
+
+
